ad9dc36909c479075fb17bac0124048f292db4d7
musaya <admin@musaya.net>
2017-06-22 20:34:53 +0200

    security, begin password forgotten

--- src/PiupiuBundle/Controller/LoginController.php
+++ src/PiupiuBundle/Controller/LoginController.php
@@ -2,12 +2,18 @@
 
 namespace PiupiuBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class LoginController extends Controller
 {
     public function loginAction()
     {
+        //todo: create form
+        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('piupiu_homepage');
+        }
+
         $helper = $this->get('security.authentication_utils');
         return $this->render('PiupiuBundle:Default:login.html.twig', [
             // last username entered by the user (if any)
@@ -15,5 +21,22 @@ class LoginController extends Controller
             // last authentication error (if any)
             'error' => $helper->getLastAuthenticationError(),
         ]);
+    }
+
+    public function pwdForgotAction(Request $request) {
+        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('piupiu_homepage');
+        }
+
+        //todo: create form
+        $form  = $this->createForm();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $order = $form->getData();
+            dump('got it'); die();
+        }
+
+        return $this->render('PiupiuBundle:Default:pwd_forgot.html.twig');
     }
 }
