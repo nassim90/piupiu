9deccc2cb15e1cb490c78ba07e22653b75ebcacc
musaya <admin@musaya.net>
2017-06-25 16:42:01 +0200

    register form continues

--- src/PiupiuBundle/Form/UserType.php
+++ src/PiupiuBundle/Form/UserType.php
@@ -3,6 +3,10 @@
 namespace PiupiuBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,15 +18,37 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
-            ->add('password')
+            ->add('username', TextType::class, [
+                'required'  => true,
+                'attr'      => ['placeholder'   => 'Your username']
+            ])
+            ->add('password', PasswordType::class, [
+                'required'  => true,
+                'attr'      => ['placeholder'   => 'Your username']
+            ])
 //            ->add('firstLogin')
-            ->add('prename')
-            ->add('surname')
-            ->add('email')
-            ->add('phone')
+            ->add('prename', TextType::class, [
+                'required'  => true,
+                'label'     => 'Firstname',
+                'attr'      => ['placeholder'   => 'Your firstname']
+            ])
+            ->add('surname', TextType::class, [
+                'required'  => true,
+                'label'     =>  'Name',
+                'attr'      => ['placeholder'   => 'Your name']
+            ])
+            ->add('email', EmailType::class, [
+                'required'  => true,
+                'attr'      => ['placeholder'   => 'Your email']
+            ])
+            ->add('phone', TextType::class, [
+                'attr'      => ['placeholder'   => 'Your phone number']
+            ])
 //            ->add('lastLoginTime')
 //            ->add('account_type');
+            ->add('submit', SubmitType::class, [
+                'label' => 'Sign up'
+            ])
         ;
     }
     
